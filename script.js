45930b0928925068ae528fde7746b9d654fab14f
Iska <isabil177@mail.ru>
2022-01-29 18:46:42 +0600

    lesson number one

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+const a = prompt('Один из просмоттренных фильмов?', ''),
+      b = prompt('на сколько оцените его?', ''),
+      c = prompt('Один из просмоттренных фильмов?', ''),
+      d = prompt('на сколько оцените его?', '');
+      
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
+
+dfksa;fdls;df
